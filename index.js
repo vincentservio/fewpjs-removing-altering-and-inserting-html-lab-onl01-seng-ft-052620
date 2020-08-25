3eb5189d0ccafdbf0ff54a1a915e066728d83c3e
Vincent Servio <vincentservio@gmail.com>
2020-08-25 22:28:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code here!
+let element = document.getElementById('main').remove();
+let newHeader = document.createElement('h1')
+newHeader.id = "victory"
+newHeader.innerHTML = `is the champion`
