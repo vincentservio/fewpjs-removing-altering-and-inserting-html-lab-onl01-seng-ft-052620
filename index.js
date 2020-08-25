79b660b422939efa6aeedbdf0ac0420ef5d80dc5
Vincent Servio <vincentservio@gmail.com>
2020-08-25 22:18:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 // Write your code here!
 let element = document.getElementById('main').remove();
-const newHeader = document.createElement('h1#victory')
+let newHeader = document.createElement('h1#victory')
